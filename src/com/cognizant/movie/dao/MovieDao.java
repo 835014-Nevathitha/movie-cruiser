578cf3cb5d330ca9a1f4e825fd1630d1be40bff2
Chandrasekaran <835014@cognizant.com>
2020-01-23 13:06:31 +0530

    java

--- src/com/cognizant/movie/dao/MovieDao.java
+++ src/com/cognizant/movie/dao/MovieDao.java
@@ -1,11 +1,9 @@
 package com.cognizant.movie.dao;
 
 import java.util.List;
-
 import com.cognizant.movie.model.Movie;
 
 public interface MovieDao {
-
     public List<Movie> getMovieListAdmin();
 
     public List<Movie> getMovieListCustomer();
